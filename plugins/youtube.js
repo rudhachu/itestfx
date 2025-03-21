ea1a62b32d057cb821f8cfd44133417a496c8216
rudhachu <120007671+rudhachu@users.noreply.github.com>
2025-03-21 16:31:45 +0530

    Update youtube.js

--- plugins/youtube.js
+++ plugins/youtube.js
@@ -1,8 +1,8 @@
-const { plugin, mode, isUrl } = require("../lib/");
+const { rudhra, mode, isUrl } = require("../lib/");
 const yts = require("yt-search");
 const fetch = require("node-fetch");
 
-plugin({
+rudhra({
     pattern: 'ytd ?(.*)',
     fromMe: mode,
     desc: 'Download audio or video from YouTube.',
@@ -80,7 +80,7 @@ plugin({
     });
 });
 
-plugin({
+rudhra({
     pattern: "song ?(.*)",
     fromMe: mode,
     desc: "Search and download audio from YouTube.",
@@ -135,7 +135,7 @@ plugin({
     }
  });
 
-plugin({
+rudhra({
   pattern: "video ?(.*)",
   fromMe: mode,
   desc: "Search and download video from YouTube.",
@@ -199,7 +199,7 @@ plugin({
   }
 });
 
-plugin({
+rudhra({
   pattern: "yta ?(.*)",
   fromMe: mode,
   desc: "Download audio from YouTube.",
@@ -235,7 +235,7 @@ plugin({
     }
 });
 
-plugin({
+rudhra({
   pattern: "ytv ?(.*)",
   fromMe: mode,
   desc: "Download video from YouTube.",
@@ -266,7 +266,7 @@ plugin({
     }
 });
 
-plugin({
+rudhra({
     pattern: "play ?(.*)",
     fromMe: mode,
     desc: "Search and download YouTube video/audio.",
@@ -346,7 +346,7 @@ plugin({
                         {
                             audio: { url: mp4 },
                             mimetype: 'audio/mpeg',
-                            fileName: `plugin-bot.mp3`,
+                            fileName: `rudhra-bot.mp3`,
                             contextInfo: {
                                 externalAdReply: {
                                     title: title,
@@ -384,7 +384,7 @@ plugin({
     }
 });
 
-plugin({
+rudhra({
   pattern: 'yts ?(.*)', 
   fromMe: mode,
   desc: 'Search for videos on YouTube.',
@@ -419,7 +419,7 @@ plugin({
                     mediaType: 1,
                     showAdAttribution: true,
                     renderLargerThumbnail: false,
-                    thumbnailUrl: "https://raw.githubusercontent.com/pluginan/media/refs/heads/main/image/yts.png"
+                    thumbnailUrl: "https://raw.githubusercontent.com/rudhraan/media/refs/heads/main/image/yts.png"
           }
         }
       };
